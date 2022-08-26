abd05a6e9c81b0f526b35a75dc30f7be3b1a32bc
sagarkarki99 <sagarkarki184@gmail.com>
2022-08-26 22:13:26 +0545

    add uuid for unique id in db

--- src/entities/AirlineEntity.ts
+++ src/entities/AirlineEntity.ts
@@ -2,7 +2,7 @@ import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class AirlineEntity {
-  @PrimaryGeneratedColumn()
+  @PrimaryGeneratedColumn('uuid')
   id: number;
 
   @Column({ unique: true })
