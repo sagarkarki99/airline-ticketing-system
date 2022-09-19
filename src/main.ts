9bcb5987aa5cf0c981dcfc5bd9b43be7a04effb7
sagarkarki99 <sagarkarki184@gmail.com>
2022-09-19 16:39:42 +0545

    add swagger config

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Airline System')
+    .setDescription('The Airline system API description')
+    .setVersion('1.0')
+    .addTag('Airline System')
+    .build();
+  const document = await SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
